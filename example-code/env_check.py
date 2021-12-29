330c6724dc1e3a56d40de4c19b9965810698b60d
Jackon Yang <i@jackon.me>
2021-12-30 01:04:46 +0800

    flake8 cleaning

--- example-code/env_check.py
+++ example-code/env_check.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 mnist = tf.keras.datasets.mnist
 
-(x_train, y_train),(x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 model = tf.keras.models.Sequential([
