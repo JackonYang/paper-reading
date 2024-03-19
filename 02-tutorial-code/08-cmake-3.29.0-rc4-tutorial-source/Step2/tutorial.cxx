1393b7a9d6853480218033b4163b7e0f8c69c40e
JackonYang <i@jackon.me>
2024-03-19 22:33:47 +0800

    cmake step2, stage 1

--- 02-tutorial-code/08-cmake-3.29.0-rc4-tutorial-source/Step2/tutorial.cxx
+++ 02-tutorial-code/08-cmake-3.29.0-rc4-tutorial-source/Step2/tutorial.cxx
@@ -5,6 +5,7 @@
 
 // TODO 5: Include MathFunctions.h
 #include "TutorialConfig.h"
+#include "MathFunctions.h"
 
 int main(int argc, char* argv[])
 {
@@ -20,9 +21,10 @@ int main(int argc, char* argv[])
   const double inputValue = std::stod(argv[1]);
 
   // TODO 6: Replace sqrt with mathfunctions::sqrt
+  const double outputValue = mathfunctions::sqrt(inputValue);
 
   // calculate square root
-  const double outputValue = sqrt(inputValue);
+  // const double outputValue = sqrt(inputValue);
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
   return 0;
