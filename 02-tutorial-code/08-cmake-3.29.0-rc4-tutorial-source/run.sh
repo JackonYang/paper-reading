263204899bc772639ba3d476aac236d5d0023975
JackonYang <i@jackon.me>
2024-03-19 22:56:05 +0800

    add cmake option

--- 02-tutorial-code/08-cmake-3.29.0-rc4-tutorial-source/run.sh
+++ 02-tutorial-code/08-cmake-3.29.0-rc4-tutorial-source/run.sh
@@ -1,11 +1,13 @@
 set -x
 
+# hint, step2: bash run.sh 2 -DUSE_MYMATH=OFF
+
 build_dir=build
 
 rm -rf $build_dir
 mkdir -p $build_dir && cd $build_dir
 
-cmake ../Step$1
+cmake ../Step$1 "${@:2}"
 
 make
 ./b.out 9
